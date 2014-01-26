36d392bbd9f3dd9ee7dfa69829848a2bb0053cb5
Matvej Kotov <mkot@mkot-st.(none)>
2014-01-26 23:29:39 +0700

    added manual evaluation of limits

--- sympy/series/__init__.py
+++ sympy/series/__init__.py
@@ -1,11 +1,11 @@
 """A module that handles series: find a limit, order the series etc.
 """
 from .order import Order
-from .limits import limit, Limit
+from .limits import limit, Limit, lim
 from .gruntz import gruntz
 from .series import series
 from .residues import residue
 
 O = Order
 
-__all__ = ['gruntz', 'limit', 'series', 'O', 'Order', 'Limit', "residue"]
+__all__ = ['gruntz', 'limit', 'series', 'O', 'Order', 'Limit', "residue", "lim"]
--- sympy/series/limits.py
+++ sympy/series/limits.py
@@ -1,9 +1,31 @@
 from __future__ import print_function, division
 
-from sympy.core import S, Symbol, Add, sympify, Expr, PoleError, Mul, oo, C
+from sympy.core import S, Symbol, Add, sympify, Expr, PoleError, C
 from sympy.core.compatibility import string_types
-from sympy.functions import tan, cot, factorial, gamma
+from sympy.functions import factorial, gamma
 from .gruntz import gruntz
+from sympy.utilities.solution import start_subroutine, commit_subroutine, cancel_subroutine
+from sympy.core.function import expand
+from sympy.core.mul import Mul, prod
+from sympy.core.singleton import S
+from sympy.core.symbol import Dummy
+from sympy.functions.elementary.exponential import exp, log
+from sympy.polys.polytools import Poly
+from sympy.utilities.solution import add_comment, add_exp, add_eq
+from sympy.core import oo
+
+
+def lim(f, x, x0, dir="+"):
+    start_subroutine("Evaluate the limit")
+    add_comment("Evaluate the limit")
+    l = _manuallimit(f, x, x0)
+    test = limit(f, x, x0, dir)
+    if l == test:
+        commit_subroutine()
+        return l
+    else:
+        cancel_subroutine()
+        return test
 
 
 def limit(e, z, z0, dir="+"):
@@ -155,3 +177,93 @@ class Limit(Expr):
             z = z.doit(**hints)
             z0 = z0.doit(**hints)
         return limit(e, z, z0, str(dir))
+
+
+
+def _min_degree(f, x):
+    md = oo
+    for t in f.as_terms()[0]:
+        if t[0].is_polynomial(x):
+            p = Poly(t[0], x)
+            d = p.degree(x)
+            if d < md:
+                md = d
+    return md
+
+
+def _is_series(f, x):
+    for t in f.as_terms()[0]:
+        if not t[0].is_polynomial(x) and not t[0].is_Order:
+            return False
+    return True
+
+def _to_series(f, x, x0):
+    term = f.series(n=None)
+    r = next(term)
+    d = Poly(r, x).degree()
+    return f.series(n=d + 1)
+
+
+def _rewrite_factors_using_series(factors, x, x0):
+    u = Dummy("u")
+    rewriten_factors = []
+    for factor in factors:
+        base, power = factor.as_base_exp()
+        if base != x and base.subs(x, x0) == 0:
+            add_comment("We know that")
+            s = _to_series(base, x, x0)
+            add_eq(base, s)
+            rewriten_factors.append(_to_series(base, x, x0) ** power)
+            continue
+        rewriten_factors.append(factor)
+    return rewriten_factors
+
+
+def _manuallimit(f, x, x0):
+    add_exp(Limit(f, x, x0))
+    v = f.subs(x, x0)
+    if not v is S.NaN:
+        add_comment("This limit is equal to")
+        add_exp(v)
+        return v
+    if x0 != oo and x0 != -oo and x0 != 0:
+        add_comment("Rewrite this limit as")
+        y = Dummy("y")
+        return _manuallimit(f.subs(x, y + x0), y, 0)
+    if f.is_Pow:
+        add_comment("Rewrite this limit as")
+        add_exp(exp(Limit(f.args[1] * log(f.args[0]), x, x0)))
+        add_comment("Evaluate the limit")
+        return exp(_manuallimit(f.args[1] * log(f.args[0]), x, x0))
+
+    if x0 == oo or x0 == -oo:
+        if f.func is Mul:
+            num, den = f.as_numer_denom()
+            if num.is_polynomial(x) and den.is_polynomial(x):
+                num = Poly(num, x)
+                den = Poly(den, x)
+                d = min(num.degree(x), den.degree(x))
+                num = expand(num / x**d)
+                den = expand(den / x**d)
+                f = num / den
+                add_comment("Rewrite this limit as")
+                return _manuallimit(f, x, x0)
+    if x0 == 0:
+        if f.func is Mul:
+            num, den = f.as_numer_denom()
+            num = expand(num)
+            den = expand(den)
+            if _is_series(num, x) and _is_series(den, x):
+                d1 = _min_degree(num, x)
+                d2 = _min_degree(den, x)
+                d = max(d1, d2)
+                num = expand(num / x**d)
+                den = expand(den / x**d)
+                f = num / den
+                add_comment("Rewrite this limit as")
+                return _manuallimit(f, x, x0)
+
+            rf = prod(_rewrite_factors_using_series(f.args, x, x0))
+            if rf != f:
+                add_comment("Rewrite this limit as")
+                return _manuallimit(rf, x, x0)
