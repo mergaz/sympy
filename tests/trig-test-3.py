74c9bd6ad9fa7c0319ec6c6d8ec91f2d4c2350e3
Alexander Kalinin <alec.kalinin@gmail.com>
2015-01-20 18:01:05 +0300

    Solve equation: a*sin(x) + b*sin(2*x) + a*sin(3*x).
    Only noted general form of equation is avialable.
    
    The equation in the form a*sin(x) + b*sin(2*x) + c*sin(3*x)
    can not be solved because we use sin(2*x) factorization. This
    factorization requires 'a' equals to 'c'.
    
    Also one SymPy parsing issue have been found and reported to
    the main SymPy repository:
    https://github.com/sympy/sympy/issues/8852

--- tests/trig-test-3.py
+++ tests/trig-test-3.py
@@ -4,6 +4,7 @@
 
 from sympy import *
 from sympy.utilities.solution import *
+from sympy.utilities.solution_log import log_as_latex, log_to_file
 
 x = Symbol('x')
 
@@ -18,10 +19,12 @@ eqs = [
     5 * cos(x) ** 2 - 5 * cos(x) + 1,
     8 * cos(x) ** 2 + 6 * sin(x) - 3,
     3 * tan(x) ** 3 + tan(x),
-    sin(3 * x) * cos(4 * x)
+    sin(3 * x) * cos(4 * x),
+    sin(x) + sin(2 * x) + sin(3 * x),
+    2 * sin(x) + 3 * sin(2 * x) + 2 * sin(3 * x)
 ]
 
-for eq in eqs:
+for i, eq in enumerate(eqs):
     print '===================================================='
     print '=== Equation: ' + latex(eq) + ' = 0'
 
@@ -33,3 +36,4 @@ for eq in eqs:
     print '=== Answer:'
     for r in res:
         print latex(r)
+    log_to_file('log{}.tex'.format(i), log_as_latex(R))
