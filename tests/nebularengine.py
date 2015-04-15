908b2914147ceeb6f45e2cd9d81a8ad5790b6423
Vaagn Khachatryan <hrashk@yandex.ru>
2015-04-16 00:42:56 +0300

    experiments with parallel testing engine

---
+++ tests/nebularengine.py
@@ -0,0 +1,59 @@
+import inspect
+import billiard as mp
+import sys
+from sympy.core.cache import clear_cache
+from sympy.core.compatibility import exec_
+
+TIMEOUT = 60
+
+TASKS = []
+
+
+def add_expectation(func):
+    TASKS.append(func)
+
+
+def load_test_files():
+    filename = 'nebulartests.py'
+    gl = {'__file__': filename, 'add_expectation': add_expectation}
+    try:
+        with open(filename) as f:
+            source = f.read()
+
+        code = compile(source, filename, "exec")
+        exec_(code, gl)
+    except (SystemExit, KeyboardInterrupt):
+        raise
+    except ImportError:
+        print("import error", sys.exc_info())
+        return
+
+    funcs = [gl[f] for f in gl.keys() if f.startswith("test_") and
+             (inspect.isfunction(gl[f]) or inspect.ismethod(gl[f])) and
+             (inspect.getsourcefile(gl[f]) == filename)]
+    for f in funcs:
+        f()
+
+def exec_expectation(exp_no):
+    TASKS[exp_no]()
+    # return
+
+def runtests():
+    load_test_files()
+
+    pool = mp.Pool(timeout=TIMEOUT, initializer=load_test_files)
+    results = [pool.apply_async(exec_expectation, args=(i,)) for i in range(len(TASKS))]
+    for r in results:
+        passed = False
+        comment = ''
+        try:
+            r.get()
+            passed = True
+        except Exception as e:
+            comment = e.message
+        print 'example,expected,actual,{},{}'.format('Passed' if passed else 'Failed', comment)
+
+
+if __name__ == '__main__':
+    mp.freeze_support()
+    runtests()
---
+++ tests/nebulartests.py
@@ -0,0 +1,49 @@
+from sympy import solve, simplify
+from sympy import Symbol, sin, cot, pi, E, Abs, tan, S, Rational, log, Eq, sqrt, cos, ln, asin, acos, acot, atan, root, \
+    And, oo, Or, Derivative, exp, dsolve, Dummy, limit, diff, Integral, integrate
+# from nebularengine import add_expectation
+
+x = Symbol("x", real=True)
+y = Symbol("y")
+z = Symbol("z")
+a = Symbol("a")
+b = Symbol("b")
+k = Dummy('k')
+n = Dummy('n')
+
+
+def solve_10():
+    assert solve([(x ** 2) - 1 >= 0, x > 2]) == x > 2
+    assert solve((S(1) / 2) ** x > S(1) / 4) == x < 2
+    assert solve(Eq(sqrt(x + 3), sqrt(5 - x))) == [S(1)]
+    assert solve(cos(x) - 1) == [2 * pi * k]
+
+
+def test_solve_10():
+    def expect0():
+        actual = solve([(x ** 2) - 1 >= 0, x > 2])
+        expected = x > 2
+        assert actual == expected, "{} ==\n{}".format(actual, expected)
+
+    add_expectation(expect0)
+
+    def expect1():
+        actual = solve((S(1) / 2) ** x > S(1) / 4)
+        expected = x < 2
+        assert actual == expected, "{} ==\n{}".format(actual, expected)
+
+    add_expectation(expect1)
+
+    def expect2():
+        actual = solve(Eq(sqrt(x + 3), sqrt(5 - x)))
+        expected = [S(1)]
+        assert actual == expected, "{} ==\n{}".format(actual, expected)
+
+    add_expectation(expect2)
+
+    def expect3():
+        actual = solve(cos(x) - 1)
+        expected = [2 * pi * k]
+        assert actual == expected, "{} ==\n{}".format(actual, expected)
+
+    add_expectation(expect3)
