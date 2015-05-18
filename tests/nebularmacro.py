d6b1056be2fac497c406b023480dac83687ea419
Vaagn Khachatryan <hrashk@yandex.ru>
2015-05-18 23:19:58 +0300

    macropy macros

--- tests/nebularmacro.py
+++ tests/nebularmacro.py
@@ -1,25 +1,29 @@
 from macropy.core.macros import *
 from macropy.core.quotes import macros, q, ast, u
 from sympy import S
-
-# macros = Macros()
+from fractions import Fraction
 
 
 @macros.expr
-def asdf(tree, **kw):
-    # addition = 10
-    # new_tree = q[ast[tree] + u[addition]]
-    # print real_repr(new_tree)
-    # print unparse(new_tree)
-    # return new_tree
-    @Walker
-    def number_search(tree, **kw):
-        if type(tree) is BinOp and type(tree.left) is Num:
-            print unparse(tree)
-            tree.left = q[S(ast[tree.left])]
-            print unparse(tree)
-
+def parallel(tree, **kw):
     new_tree = number_search.recurse(tree)
-    print real_repr(new_tree)
-    print unparse(new_tree)
+    # print unparse(new_tree)
     return new_tree
+
+
+@Walker
+def number_search(tree, **kw):
+    if type(tree) is BinOp:
+        if type(tree.left) is Num:
+            tree.left = num2S(tree.left.n)
+        if type(tree.right) is Num:
+            tree.right = num2S(tree.right.n)
+        # print unparse(tree)
+
+
+def num2S(n):
+    frac = Fraction(n).limit_denominator()
+    if frac.denominator != 1:
+        return q[S(u[frac.numerator]) / u[frac.denominator]]
+    else:
+        return q[S(u[frac.numerator])]
--- tests/nebulartests.py
+++ tests/nebulartests.py
@@ -1,21 +1,20 @@
-from nebularmacro import macros, asdf
-from sympy import S
+from nebularmacro import macros, parallel
+
+from sympy import solve, simplify
+from sympy import Symbol, sin, cot, pi, E, Abs, tan, S, Rational, log, Eq, sqrt, cos, ln, asin, acos, acot, atan, root, \
+    And, oo, Or, Derivative, exp, dsolve, Dummy, limit, diff, Integral, integrate
+
+x = Symbol("x", real=True)
+y = Symbol("y")
+z = Symbol("z")
+a = Symbol("a")
+b = Symbol("b")
+k = Dummy('k')
+n = Dummy('n')
 
 def omicron():
-    return asdf[1-1.0/3]
+    return parallel[3 * (x ** 2) - 2 * x + (x + 0.1) * (x + 6.3) - 4/7 * (x + 0.9) * (x - 3.2)]
 
-# from sympy import solve, simplify
-# from sympy import Symbol, sin, cot, pi, E, Abs, tan, S, Rational, log, Eq, sqrt, cos, ln, asin, acos, acot, atan, root, \
-#     And, oo, Or, Derivative, exp, dsolve, Dummy, limit, diff, Integral, integrate
-#
-# x = Symbol("x", real=True)
-# y = Symbol("y")
-# z = Symbol("z")
-# a = Symbol("a")
-# b = Symbol("b")
-# k = Dummy('k')
-# n = Dummy('n')
-#
 # def test_solve_10():
 #     expect[solve([(x ** 2) - 1 >= 0, x > 2]) == x > 2]
 #     expect[solve((S(1) / 2) ** x > S(1) / 4) == x < 2]
