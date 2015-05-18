0c33bb734d9991509b4cd0eb03794c17d3ee98ba
Vaagn Khachatryan <hrashk@yandex.ru>
2015-05-18 10:19:19 +0300

    macropy macros

--- tests/nebularengine.py
+++ tests/nebularengine.py
@@ -1,60 +1,56 @@
-import inspect
-import billiard as mp
-import sys
-from sympy.core.compatibility import exec_
-
-TIMEOUT = 10
-
-
-# def add_expectation(func):
-# TASKS.append(func)
-#
-
-TASKS = []
-
-
+import macropy.activate
 import nebulartests
-def load_test_files():
-    # gl = {'__file__': filename}
-    # try:
-    # with open(filename) as f:
-    #         source = f.read()
-    #
-    #     code = compile(source, filename, "exec")
-    #     exec_(code, gl)
-    # except (SystemExit, KeyboardInterrupt):
-    #     raise
-    # except ImportError:
-    #     print("import error", sys.exc_info())
-    #     return
+print nebulartests.omicron()
 
-    funcs = [f for name, f in inspect.getmembers(nebulartests) if name.startswith("test_") and
-             inspect.isgeneratorfunction(f)]
-    for f in funcs:
-        for expectation in f():
-            TASKS.append(expectation)
-
-
-def exec_expectation(exp_no):
-    TASKS[exp_no]()
-
-
-def runtests():
-    load_test_files()
-
-    pool = mp.Pool(timeout=TIMEOUT, initializer=load_test_files)
-    results = [pool.apply_async(exec_expectation, args=(i,)) for i in range(len(TASKS))]
-    for r in results:
-        passed = False
-        comment = ''
-        try:
-            r.get()
-            passed = True
-        except Exception as e:
-            comment = e
-        print 'example,expected,actual,{},{}'.format('Passed' if passed else 'Failed', comment)
-
-
-if __name__ == '__main__':
-    mp.freeze_support()
-    runtests()
+# import inspect
+# import billiard as mp
+#
+# TIMEOUT = 10
+#
+# TASKS = []
+#
+#
+# def load_test_files():
+#     # gl = {'__file__': filename}
+#     # try:
+#     # with open(filename) as f:
+#     #         source = f.read()
+#     #
+#     #     code = compile(source, filename, "exec")
+#     #     exec_(code, gl)
+#     # except (SystemExit, KeyboardInterrupt):
+#     #     raise
+#     # except ImportError:
+#     #     print("import error", sys.exc_info())
+#     #     return
+#
+#     funcs = [f for name, f in inspect.getmembers(nebulartests) if name.startswith("test_") and
+#              inspect.isgeneratorfunction(f)]
+#     for f in funcs:
+#         for expectation in f():
+#             TASKS.append(expectation)
+#
+#
+# def exec_expectation(exp_no):
+#     TASKS[exp_no]()
+#
+#
+# def runtests():
+#     load_test_files()
+#
+#     pool = mp.Pool(timeout=TIMEOUT, initializer=load_test_files)
+#     results = [pool.apply_async(exec_expectation, args=(i,)) for i in range(len(TASKS))]
+#     for r in results:
+#         passed = False
+#         comment = ''
+#         try:
+#             r.get()
+#             passed = True
+#         except Exception as e:
+#             comment = e
+#         print 'example,expected,actual,{},{}'.format('Passed' if passed else 'Failed', comment)
+#
+#
+# if __name__ == '__main__':
+#     mp.freeze_support()
+#     # runtests()
--- tests/nebularmacro.py
+++ tests/nebularmacro.py
@@ -1,11 +1,25 @@
 from macropy.core.macros import *
-from macropy.core.quotes import q, ast
+from macropy.core.quotes import macros, q, ast, u
+from sympy import S
 
-macros = Macros()
+# macros = Macros()
+
 
 @macros.expr
-def expect(tree, gen_sym, **kw):
-    with q as code:
-        u = lambda: ast[tree]
-    name = gen_sym()
-    return code
+def asdf(tree, **kw):
+    # addition = 10
+    # new_tree = q[ast[tree] + u[addition]]
+    # print real_repr(new_tree)
+    # print unparse(new_tree)
+    # return new_tree
+    @Walker
+    def number_search(tree, **kw):
+        if type(tree) is BinOp and type(tree.left) is Num:
+            print unparse(tree)
+            tree.left = q[S(ast[tree.left])]
+            print unparse(tree)
+
+    new_tree = number_search.recurse(tree)
+    print real_repr(new_tree)
+    print unparse(new_tree)
+    return new_tree
