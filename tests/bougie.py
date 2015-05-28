8b8791e7f0ac54765f340df7c3f6d3eed853bedc
Vaagn Khachatryan <hrashk@yandex.ru>
2015-05-28 13:16:47 +0300

    links to prospective root cause of NoneType error

--- tests/bougie.py
+++ tests/bougie.py
@@ -7,3 +7,7 @@ t = Dummy('t')
 iv = _solve(2 ** x - t, x, tsolve=True, check=False)
 print iv
 # print solve(2**x - 8)
+# problems with NoneType not iterable are coming mostly from this change:
+# https://github.com/sympy/sympy/commit/9b1f3d7402538c587a00fa4df162208998727830#diff-e2a46e260ac6a40415353a416f21fd4f
+# see the full history at around 16 Jan
+# https://github.com/sympy/sympy/commits/master/sympy/solvers/solvers.py
